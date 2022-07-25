f8ac8a1d7bedd8c7b62188a60ad1e94a4d538d9d
ardithyseni <ah49393@ubt-uni.net>
2022-07-25 18:58:39 +0200

    added custom 404 page

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.main')
+
+@section('content')
+    
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 page-not-found">
+                <h2>Page not found.</h2>
+                <p>
+                    Sorry, the page you're looking for does not exist.
+                </p>
+            </div>
+        </div>
+    </div>
+
+@endsection
